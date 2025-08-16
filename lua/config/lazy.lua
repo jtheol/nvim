e464f1a47ab80301445d0680317a412766504861
Johnny <jl.johnnylowe@gmail.com>
2025-08-16 12:32:14 -0400

    Add default font.

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,6 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 vim.api.nvim_set_option("clipboard", "unnamedplus")
+vim.o.guifont = "Cascadia Code"
 
 require("lazy").setup({
   spec = {
