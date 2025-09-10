0f4dac118bfe30c4744fe96d6a6e6b109e44f9b1
Johnny <jl.johnnylowe@gmail.com>
2025-09-10 10:58:15 -0400

    Add colorscheme.lua and set default colorscheme to catppuccin.

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+  { "rose-pine/neovim" },
+  { "EdenEast/nightfox.nvim" },
+
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
