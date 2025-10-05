b5f3d35c240712f3f6d768394b8b1eb7034509c4
Johnny <jl.johnnylowe@gmail.com>
2025-10-05 17:38:15 -0400

    Add zenbones colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,12 +2,26 @@ return {
   { "rose-pine/neovim" },
   { "EdenEast/nightfox.nvim" },
   { "datsfilipe/vesper.nvim" },
+  {
+    "zenbones-theme/zenbones.nvim",
+    -- Optionally install Lush. Allows for more configuration or extending the colorscheme
+    -- If you don't want to install lush, make sure to set g:zenbones_compat = 1
+    -- In Vim, compat mode is turned on as Lush only works in Neovim.
+    dependencies = "rktjmp/lush.nvim",
+    lazy = false,
+    priority = 1000,
+    -- you can set set configuration options here
+    -- config = function()
+    --     vim.g.zenbones_darken_comments = 45
+    --     vim.cmd.colorscheme('zenbones')
+    -- end
+  },
 
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "vesper",
+      colorscheme = "zenbones",
     },
   },
 }
