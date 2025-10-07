961d8ca079734b2d213f0d4452f9b5e205309068
Johnny <jl.johnnylowe@gmail.com>
2025-10-07 09:36:57 -0400

    Update colorscheme.

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,19 +12,12 @@ return {
     "metalelf0/black-metal-theme-neovim",
     lazy = false,
     priority = 1000,
-    config = function()
-      require("black-metal").setup({
-        -- optional configuration here
-      })
-      require("black-metal").load()
-    end,
   },
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "immortal",
+      colorscheme = "darkthrone",
     },
   },
 }
