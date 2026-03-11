bddea7d9cfd633d559c9af8e6421e877d51bd4d6
Johnny <jl.johnnylowe@gmail.com>
2026-03-10 20:05:50 -0400

    testing

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -8,6 +8,24 @@ return {
         typescript = { "biome-organize-imports", "biome" },
         typescriptreact = { "biome-organize-imports", "biome" },
       },
+      formatters = {
+        biome = { require_cwd = true },
+        ["biome-organize-imports"] = { require_cwd = true },
+      },
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        eslint = { enabled = false },
+        vtsls = {
+          on_attach = function(client)
+            client.server_capabilities.documentFormattingProvider = false
+          end,
+        },
+      },
     },
   },
 }
