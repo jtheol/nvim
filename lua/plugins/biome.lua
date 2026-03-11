b6fdedd2064244ec258176c14021613b28c4a0a7
Johnny <jl.johnnylowe@gmail.com>
2026-03-10 20:19:47 -0400

    testing.

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -2,11 +2,18 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      format_on_save = {
+        timeout_ms = 3000,
+        lsp_format = "fallback",
+      },
       formatters_by_ft = {
         javascript = { "biome-organize-imports", "biome" },
         javascriptreact = { "biome-organize-imports", "biome" },
         typescript = { "biome-organize-imports", "biome" },
         typescriptreact = { "biome-organize-imports", "biome" },
+        json = { "biome" },
+        jsonc = { "biome" },
+        css = { "biome" },
       },
       formatters = {
         biome = { require_cwd = true },
@@ -19,7 +26,11 @@ return {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
-        eslint = { enabled = false },
+        eslint = {
+          on_attach = function(client)
+            client.server_capabilities.documentFormattingProvider = false
+          end,
+        },
         vtsls = {
           on_attach = function(client)
             client.server_capabilities.documentFormattingProvider = false
