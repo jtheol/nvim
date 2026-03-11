0471c3f57c40f6d01fed6ec98cc6a7c8ccfc8cff
Johnny <jl.johnnylowe@gmail.com>
2026-03-10 20:20:45 -0400

    Fix

--- lua/plugins/biome.lua
+++ lua/plugins/biome.lua
@@ -2,18 +2,11 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
-      format_on_save = {
-        timeout_ms = 3000,
-        lsp_format = "fallback",
-      },
       formatters_by_ft = {
         javascript = { "biome-organize-imports", "biome" },
         javascriptreact = { "biome-organize-imports", "biome" },
         typescript = { "biome-organize-imports", "biome" },
         typescriptreact = { "biome-organize-imports", "biome" },
-        json = { "biome" },
-        jsonc = { "biome" },
-        css = { "biome" },
       },
       formatters = {
         biome = { require_cwd = true },
